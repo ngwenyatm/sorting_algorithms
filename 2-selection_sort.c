05e2fa64e1519dce974e6ba30a1c780a2672364b
Thembisile  Makamu <127257209+MakamuT@users.noreply.github.com>
2024-03-04 01:21:43 +0200

    Create 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: int
+ * @size: number of elements
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+    size_t = n, m, o;
+    for (n = 0; n < size - 1; n++)
+        {
+            size_t min_index = n;
+              for (m = n; m < size;m++){
+                if (array[m[ < array[min_index]){
+                  min_index = m;
+                }
+              }
+if (min_index != n){
+swap(&array[n], &array[min_index]);
+printf("After swapping: ");
+
+for (o = 0; o < size; o++){
+printf("%d ", array[o]);
+}
+printf("\n")
+  }
+        }
+}
+            
+
