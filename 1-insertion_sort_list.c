60d007c45fdd39cbf73a40a477d759f156dc6bef
ngwenyatm <thabsnspaces@gmail.com>
2024-03-04 10:15:43 -0800

    pushed with script

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,31 +5,33 @@
  *
  * Description: Print dll  after each time swapped.
  */
-void insertion_sort_list(listint_t **list) {
-    listint_t *temp, *current;
-	if (*list == NULL || (*list)->next == NULL) {
-        return;
-    }
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *current;
 
-    current = (*list)->next;
-
-    while (current) {
-        while (current->prev && current->prev->n > current->n) {
-            current = current->prev;
-        }
-
-        temp = current->next;
-        current->next = current->prev;
-
-        if (current->prev) {
-            current->prev->next = current;
-        } else {
-            
-            *list = current;
-        }
-
-        current = temp;
-    }
-    print_list(current);
-    printf("\n");
+	if (*list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+	current = (*list)->next;
+	while (current)
+	{
+		while (current->prev && current->prev->n > current->n)
+		{
+			current = current->prev;
+		}
+		temp = current->next;
+		current->next = current->prev;
+		if (current->prev)
+		{
+			current->prev->next = current;
+		}
+		else
+		{
+			*list = current;
+		}
+		current = temp;
+	}
+	print_list(current);
+	printf("\n");
 }
