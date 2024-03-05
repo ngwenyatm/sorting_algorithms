d71a36e457555f1aad977b44dcea200f79b98fc3
ngwenyatm <thabsnspaces@gmail.com>
2024-03-05 02:23:03 -0800

    pushed with script

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,38 +6,36 @@
  * Description: Print dll  after each time swapped
  */
 void insertion_sort_list(listint_t **list) {
-    listint_t *temp, *sorted = NULL;
+  listint_t *temp, *sorted = NULL;
 
-    while (*list) {
-        temp = *list;
-        *list = temp->next;
+  while (*list) {
+    temp = *list;
+    *list = temp->next;
 
-        if (!sorted || sorted->prev->n <= temp->n) {
-            
-            temp->next = NULL;
-            if (sorted) {
-                sorted->prev->next = temp;
-            } else {
-                *list = temp;
-            }
-            temp->prev = sorted->prev;
-            sorted = temp;
-        } else {
-           
-            while (sorted && sorted->n < temp->n) {
-                sorted = sorted->next;
-            }
+    if (!sorted || sorted->prev->n <= temp->n) {
+      temp->next = NULL;
+      if (sorted) {
+        sorted->prev->next = temp;
+      } else {
+        *list = temp;
+      }
+      temp->prev = sorted->prev;
+      sorted = temp;
+    } else {
+      while (sorted && sorted->n < temp->n) {
+        sorted = sorted->next;
+      }
 
-            temp->next = sorted;
-            if (sorted) {
-                sorted->prev->next = temp;
-            }
-            temp->prev = sorted->prev;
-            if (!sorted) {
-                *list = temp;
-            }
-            sorted->prev = temp;
-        }
+      temp->next = sorted;
+      if (sorted) {
+        sorted->prev->next = temp;
+      }
+      temp->prev = sorted->prev;
+      if (!sorted) {
+        *list = temp;
+      }
+      sorted->prev = temp;
     }
-    *list = sorted;
+  }
+  *list = sorted;
 }
