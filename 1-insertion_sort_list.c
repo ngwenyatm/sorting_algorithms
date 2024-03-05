d1170aa49cbbc5766ab46a501fc605d8066ed542
ngwenyatm <thabsnspaces@gmail.com>
2024-03-05 02:02:53 -0800

    pushed with script

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,36 +3,35 @@
  * insertion_sort_list - Sorts DLL of integers with insertion sort
  * @list: A pointer to head of DLL
  *
- * Description: Print dll  after each time swapped.
+ * Description: Print dll  after each time swapped
  */
-void insertion_sort_list(listint_t **list)
-{
+void insertion_sort_list(listint_t **list) {
     listint_t *temp, *sorted = NULL;
 
     while (*list) {
         temp = *list;
         *list = temp->next;
 
-
+        
         while (sorted && sorted->n < temp->n) {
             sorted = sorted->next;
         }
 
-
+        
         if (sorted) {
             temp->next = sorted;
             temp->prev = sorted->prev;
             if (sorted->prev) {
                 sorted->prev->next = temp;
-            } else {
+            } else { 
                 *list = temp;
             }
             sorted->prev = temp;
-        } else {
+        } else { 
             temp->next = NULL;
             temp->prev = NULL;
             sorted = temp;
+        }
     }
     *list = sorted;
-}
 }
