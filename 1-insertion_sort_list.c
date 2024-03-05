74f1c095f269ff61b0ffab52b82b082ec40a4acc
ngwenyatm <thabsnspaces@gmail.com>
2024-03-05 01:32:41 -0800

    pushed with script

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,31 +7,32 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *sorted = NULL;
+    listint_t *temp, *sorted = NULL;
 
-	while (*list)
-	{
-		temp = *list;
-		*list = temp->next;
-		while (sorted && sorted->n < temp->n)
-		{
-			sorted = sorted->next;
-		}
-		
-		if (sorted)
-		{
-			temp->next = sorted;
-			temp->prev = sorted->prev;
-			sorted->prev->next = temp;
-			sorted->prev = temp;
-		}
-		else
-		{
-			temp->next = sorted;
-			temp->prev = NULL;
-			*list = temp;
-		}
-		print_list(*list);
-		*list = sorted;
-	}
+    while (*list) {
+        temp = *list;
+        *list = temp->next;
+
+
+        while (sorted && sorted->n < temp->n) {
+            sorted = sorted->next;
+        }
+
+
+        if (sorted) {
+            temp->next = sorted;
+            temp->prev = sorted->prev;
+            if (sorted->prev) {
+                sorted->prev->next = temp;
+            } else {
+                *list = temp;
+            }
+            sorted->prev = temp;
+        } else {
+            temp->next = NULL;
+            temp->prev = NULL;
+            sorted = temp;
+    }
+    *list = sorted;
+}
 }
