68ed4b4c9f817ada5a463c5e1951fda93c2ead02
ngwenyatm <thabsnspaces@gmail.com>
2024-03-06 01:16:27 -0800

    pushed with script

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,10 +7,9 @@
  */
 void swap(int *a, int *b)
 {
-        int temp = *a;
-
-        *a = *b;
-        *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 /**
  * partition - divides the array
@@ -21,20 +20,20 @@ void swap(int *a, int *b)
  */
 int partition(int *array, int low, int high)
 {
-        int piv = array[high];
-        int a = low - 1;
-        int i;
+	int piv = array[high];
+	int a = low - 1;
+	int i;
 
-        for (i = low; i < high; i++)
-        {
-                if (array[i] < piv)
-                {
-                        a++;
-                        swap(&array[a], &array[i]);
-                }
-        }
-        swap(&array[a + 1], &array[high]);
-        return (a + 1);
+	for (i = low; i < high; i++)
+	{
+		if (array[i] < piv)
+		{
+			a++;
+			swap(&array[a], &array[i]);
+		}
+	}
+	swap(&array[a + 1], &array[high]);
+	return (a + 1);
 }
 /**
  * quick_sort_assit - sorts the array
@@ -45,14 +44,14 @@ int partition(int *array, int low, int high)
  */
 void quick_sort_assit(int *array, int low, int high)
 {
-        if (low < high)
-        {
-                int partition_ind = partition(array, low, high);
+	if (low < high)
+	{
+		int partition_ind = partition(array, low, high);
 
-                print_array(array, high - low + 1);
-                quick_sort_assit(array, low, partition_ind - 1);
-                quick_sort_assit(array, partition_ind + 1, high);
-        }
+		print_array(array, high - low + 1);
+		quick_sort_assit(array, low, partition_ind - 1);
+		quick_sort_assit(array, partition_ind + 1, high);
+	}
 }
 /**
  * quick_sort - sorts an array of integers in ascending order
@@ -62,5 +61,5 @@ void quick_sort_assit(int *array, int low, int high)
  */
 void quick_sort(int *array, size_t size)
 {
-        quick_sort_assit(array, 0, (size - 1));
+	quick_sort_assit(array, 0, (size - 1));
 }
