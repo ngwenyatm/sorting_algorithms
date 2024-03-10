8a1c2ac9de1fac59d1d1466ee8beb5ca170dfe5f
Thembisile  Makamu <127257209+MakamuT@users.noreply.github.com>
2024-03-10 08:39:50 +0200

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,13 +13,13 @@ void swap(int *a, int *b)
         *b = temp;
 }
 /**
- * lomuto_partition - divides the array
+ * partition - divides the array
  * @array: int
  * @low: pivot element
  * @high: pivot element
  * Return: array
  */
-int lomuto_partition(int *array, int low, int high)
+int partition(int *array, int low, int high)
 {
         int pivot = array[high];
         int a = low;
@@ -37,21 +37,21 @@ int lomuto_partition(int *array, int low, int high)
  return (a + 1);
 }
 /**
- * lomjuto_sort - sorts the array
+ * quick_sort - sorts the array
  * @array: int
  * @low: pivot element
  * @high: pivot element
  * Return: array
  */
-void lomuto_sort(int *array, int low, int high)
+void quick_sort_assist(int *array, int low, int high)
 {
         if (low < high)
         {
-                int partition_ind = lomuto_partition(array, low, high);
+                int partition_ind = partition(array, low, high);
 
                 print_array(array, high - low + 1);
-                lomuto_sort(array, low, partition_ind - 1);
-                lomuto_sort(array, partition_ind + 1, high);
+                quick_sort(array, low, partition_ind - 1);
+                quick_sort(array, partition_ind + 1, high);
         }
 }
 /**
@@ -62,5 +62,5 @@ void lomuto_sort(int *array, int low, int high)
  */
 void quick_sort(int *array, size_t size)
 {
-        quick_sort_assit(array, 0, size - 1);
+        quick_sort_assist(array, 0, size - 1);
 }
